f342ea81897a67c7e28f0dd5667922384c5dc830
Tombbbb <77402108+Tombbbb@users.noreply.github.com>
2022-02-09 11:23:38 +0000

    Add files via upload

---
+++ GMM.py
@@ -0,0 +1,87 @@
+import numpy as np
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+import pandas as pd
+import wake_model
+import math
+
+def pol2cart(rho, phi):
+    x = rho * np.cos(np.pi*(phi/180))
+    y = rho * np.sin(np.pi*(phi/180))
+    return[x, y]
+
+def cost(L):
+    Turbine_count = 0
+    for i in L:
+        if i == True:
+            Turbine_count += 1
+    C = Turbine_count*((2/3) + (1/3)*(math.e**(-0.00174*((Turbine_count)**2))))
+    return C
+
+dataset = []
+
+num_lines = sum(1 for line in open('wind_data_combined.txt'))
+f = open("wind_data_combined.txt", "r")
+f.readline()
+count=1
+for i in range(num_lines):
+    line = f.readline()
+    try:
+        line = line.split(",")
+        line[0] = float(line[0])
+        line[1] = float(line[1][:-1])
+        dataset.append(pol2cart(line[0],line[1]))
+    except:
+        continue
+    count+=1
+f.close()
+
+X = np.array(dataset)
+gmm = GaussianMixture(n_components=2).fit(X)
+labels = gmm.predict(X)
+frame = pd.DataFrame(X)
+
+
+frame['cluster'] = labels
+frame.columns = ['Weight', 'Height', 'cluster']
+
+wind = 10
+divisions = 72
+
+cart_values = []
+pol_values = []
+for i in range(0, divisions):
+    cart_values.append(pol2cart(wind, i*360/divisions))
+    pol_values.append([wind, i*360/divisions])
+cart_values = np.array(cart_values)
+
+weights = gmm.predict_proba(cart_values)
+density = []
+for i in weights:
+    density.append(max(i[0],i[1]))
+total = sum(density)
+for i in range(0,len(density)):
+    density[i] = density[i]/total
+
+
+T = np.full((400), False)
+for i in range(400):
+    T[i] = True
+
+AveragePowerOutput = 0
+for i in range(0, divisions):
+    AveragePowerOutput += wake_model.Wake(T, wind, i*360/divisions)*density[i]
+AveragePowerOutput = AveragePowerOutput/divisions
+print(AveragePowerOutput)
+wake_model.print_wind_farm(T)
+
+Turbine_cost = cost(T)
+print(Turbine_cost)
+
+'''
+color=['blue','green']
+for k in range(0,2):
+    X = frame[frame["cluster"]==k]
+    plt.scatter(X["Weight"],X["Height"],c=color[k])
+plt.show()
+'''
---
+++ wake_model.py
@@ -0,0 +1,106 @@
+import math
+
+class Turbines:
+    def __init__(self):
+        self.Positions = []
+        self.r = 41  #rotor radius
+        self.h_height = 80    #hub height
+
+    def addTurbine(self, x, y):
+        self.Positions.append([x,y])
+
+    def getTurbines(self):
+        return self.Positions
+
+    def getRadius(self):
+        return self.r
+
+
+def getAngle(x, y, z):
+    '''
+    returns the angle created between 3 points
+    '''
+    angle = math.degrees(math.atan2(z[1]-y[1], z[0]-y[0]) - math.atan2(x[1]-y[1], x[0]-y[0]))
+    if angle < 0:
+        angle += 360
+    return angle
+
+def getWindFarmList():
+    wind_farm = []
+    for i in range(0,20):
+        wind_farm.append([None]*20)
+    return wind_farm
+
+
+def Wake(turbine_string, wind_speed, wind_direcetion):
+
+    Ct = 0.8 #Thrust Coefficient
+    Kw = 0.2 #Wake Decay Coefficient
+    a = 103.33
+    m = 20.53
+    n = 1190.73
+    tau = 1.14
+    VP = [a,m,n,tau] #vector parameter of the logistic function
+
+    wind_farm = getWindFarmList()
+
+    T = Turbines()
+    for i in range(400):
+        if turbine_string[i] == True:
+            T.addTurbine(math.floor(i/20), i % 20)
+
+    turbine_list = T.getTurbines()
+
+    for i in turbine_list:
+        coords = i
+        wind_farm[coords[0]][coords[1]] = i
+
+    Turbine_power = 0
+    for i in turbine_list:
+        Turbine_A = i
+        Turbine_wake = 0
+        Turbine_wake_list = []
+        for j in turbine_list:
+            if i != j:
+                Turbine_B = j
+                Turbine_angle = getAngle([Turbine_B[0]+1,Turbine_B[1]], Turbine_B, Turbine_A)
+                wake_range = math.degrees(math.atan(Kw))
+                if wind_direcetion + wake_range >= Turbine_angle and wind_direcetion - wake_range <= Turbine_angle:
+                    diff = math.sqrt(((Turbine_A[0]-Turbine_B[0])*246)**2 + ((Turbine_A[1]-Turbine_B[1])*246)**2)
+                    x = diff*math.cos(math.radians(Turbine_angle - wind_direcetion))
+                    SD = (1-math.sqrt(1-Ct))/((1+(Kw*x)/(T.getRadius()))**2)
+                    Turbine_wake_list.append(SD)
+        if len(Turbine_wake_list) == 0:
+            Turbine_wake = wind_speed
+        else:
+            for k in Turbine_wake_list:
+                 Turbine_wake += k**2
+            Turbine_wake = wind_speed*(1 - math.sqrt(Turbine_wake))
+        Turbine_power += a*(1+m*(math.e**(0-(Turbine_wake/(T.getRadius())))))/(1+n*(math.e**(0-(Turbine_wake/(T.getRadius())))))
+
+    return Turbine_power
+
+
+def print_wind_farm(turbine_string):
+
+    wind_farm = getWindFarmList()
+
+    T = Turbines()
+    for i in range(400):
+        if turbine_string[i] == True:
+            T.addTurbine(math.floor(i/20), i % 20)
+
+    turbine_list = T.getTurbines()
+    for i in turbine_list:
+        coords = i
+        wind_farm[coords[0]][coords[1]] = i
+
+    for i in wind_farm:
+        row = ""
+        for j in i:
+            if j != None:
+                row += "O"
+            else:
+                row += "."
+        print(row)
+    return
